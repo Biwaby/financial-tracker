e21af49cd9687ba404b108b12ad6e7a2733bbaaa
Andrey <456kam00@gmail.com>
2025-03-10 23:28:18 +0300

    feature: add wallet limit rest controller

---
+++ src/main/java/com/biwaby/financialtracker/controller/user/LimitController.java
@@ -0,0 +1,77 @@
+package com.biwaby.financialtracker.controller.user;
+
+import com.biwaby.financialtracker.dto.response.DeleteResponse;
+import com.biwaby.financialtracker.dto.response.EditResponse;
+import com.biwaby.financialtracker.dto.response.ObjectListResponse;
+import com.biwaby.financialtracker.dto.response.ObjectResponse;
+import com.biwaby.financialtracker.entity.Limit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/limits")
+@RequiredArgsConstructor
+public class LimitController {
+
+    @PostMapping("/add")
+    public ResponseEntity<ObjectResponse> add(
+            @RequestBody Limit limit
+    ) {
+        ObjectResponse response = new ObjectResponse(
+                "Limit added successfully",
+                HttpStatus.OK.toString(),
+                null
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get-by-id")
+    public ResponseEntity<ObjectResponse> getById(
+            @RequestParam Long id
+    ) {
+        ObjectResponse response = new ObjectResponse(
+                "Limit with id %s".formatted(id),
+                HttpStatus.OK.toString(),
+                null
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<ObjectListResponse> getAll() {
+        ObjectListResponse response = new ObjectListResponse(
+                "Limits list",
+                HttpStatus.OK.toString(),
+                null
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<EditResponse> edit(
+            @RequestParam Long id,
+            @RequestBody Limit limit
+    ) {
+        EditResponse response = new EditResponse(
+                "Limit with id %s has been successfully edited".formatted(id),
+                HttpStatus.OK.toString(),
+                null,
+                null
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<DeleteResponse> deleteById(
+            @RequestParam Long id
+    ) {
+        DeleteResponse response = new DeleteResponse(
+                "Limit with id %s has been successfully deleted".formatted(id),
+                HttpStatus.OK.toString(),
+                null
+        );
+        return ResponseEntity.ok(response);
+    }
+}
