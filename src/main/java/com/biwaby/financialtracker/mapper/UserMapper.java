0e2f6f3f2b14c9aa70a560ceaede80d42c9ac947
Andrey <456kam00@gmail.com>
2025-03-13 23:22:11 +0300

    feature: add user mapper

---
+++ src/main/java/com/biwaby/financialtracker/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.biwaby.financialtracker.mapper;
+
+import com.biwaby.financialtracker.dto.UserDto;
+import com.biwaby.financialtracker.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserMapper {
+
+    UserDto toDto(User user);
+}
