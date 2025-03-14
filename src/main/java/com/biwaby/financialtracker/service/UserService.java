e9c7d3b1e891b610dc69b0cc30870dccd004dfa5
Andrey <456kam00@gmail.com>
2025-03-14 12:29:48 +0300

    feature: add new methods in user service

--- src/main/java/com/biwaby/financialtracker/service/UserService.java
+++ src/main/java/com/biwaby/financialtracker/service/UserService.java
@@ -2,17 +2,24 @@ package com.biwaby.financialtracker.service;
 
 import com.biwaby.financialtracker.dto.UserDto;
 import com.biwaby.financialtracker.dto.UserEditDto;
+import com.biwaby.financialtracker.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
 public interface UserService {
 
+    User save(User user);
+    User create(User user);
     UserDto getById(Long id);
     UserDto getByUsername(String username);
+    User getEntityByUsername(String username);
+    User getCurrentUserEntity();
     UserDto getSelf();
     List<UserDto> getAll(Integer pageSize, Integer pageNumber);
     UserEditDto editSelf(UserEditDto userEditDto);
     void deleteById(Long id);
     void deleteByUsername(String username);
     void deleteSelf();
+    UserDetailsService getUserDetailsService();
 }
--- src/main/java/com/biwaby/financialtracker/service/impl/UserServiceImpl.java
+++ src/main/java/com/biwaby/financialtracker/service/impl/UserServiceImpl.java
@@ -7,9 +7,13 @@ import com.biwaby.financialtracker.exception.ResponseException;
 import com.biwaby.financialtracker.mapper.UserMapper;
 import com.biwaby.financialtracker.repository.UserRepository;
 import com.biwaby.financialtracker.service.UserService;
+import com.biwaby.financialtracker.util.PasswordEncoderUtil;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,8 +26,22 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    private User save(User user) {
+    @Override
+    @Transactional
+    public User save(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public User create(User user) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new ResponseException(
+                    HttpStatus.BAD_REQUEST.value(),
+                    "User with username %s already exists".formatted(user.getUsername())
+            );
+        }
+        return save(user);
     }
 
     @Override
@@ -50,10 +68,25 @@ public class UserServiceImpl implements UserService {
         );
     }
 
-    // TODO: getSelf() - UserServiceImpl
+    @Override
+    public User getEntityByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(
+                () -> new ResponseException(
+                        HttpStatus.NOT_FOUND.value(),
+                        "User with username %s is not found".formatted(username)
+                )
+        );
+    }
+
+    @Override
+    public User getCurrentUserEntity() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getEntityByUsername(username);
+    }
+
     @Override
     public UserDto getSelf() {
-        return null;
+        return userMapper.toDto(getCurrentUserEntity());
     }
 
     @Override
@@ -64,13 +97,19 @@ public class UserServiceImpl implements UserService {
                 .collect(Collectors.toList());
     }
 
-    // TODO: editSelf() - UserServiceImpl
     @Override
+    @Transactional
     public UserEditDto editSelf(UserEditDto userEditDto) {
-        return null;
+        User user = getCurrentUserEntity();
+        user.setUsername(userEditDto.getUsername());
+        user.setPassword(PasswordEncoderUtil.getPasswordEncoder()
+                .encode(userEditDto.getPassword())
+        );
+        return userMapper.toEditDto(save(user));
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         User userToDelete = userRepository.findById(id).orElseThrow(
                 () -> new ResponseException(
@@ -82,6 +121,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void deleteByUsername(String username) {
         User userToDelete = userRepository.findByUsername(username).orElseThrow(
                 () -> new ResponseException(
@@ -92,9 +132,15 @@ public class UserServiceImpl implements UserService {
         userRepository.delete(userToDelete);
     }
 
-    // TODO: deleteSelf() - UserServiceImpl
     @Override
+    @Transactional
     public void deleteSelf() {
+        User userToDelete = getCurrentUserEntity();
+        userRepository.delete(userToDelete);
+    }
 
+    @Override
+    public UserDetailsService getUserDetailsService() {
+        return this::getEntityByUsername;
     }
 }
