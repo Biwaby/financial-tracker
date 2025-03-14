7c53e20c6da23aac8036a4eada4f3bfab886c069
Andrey <456kam00@gmail.com>
2025-03-14 11:51:15 +0300

    refactor: add exception class name in ResponseException

--- src/main/java/com/biwaby/financialtracker/controller/advice/ExceptionsHandler.java
+++ src/main/java/com/biwaby/financialtracker/controller/advice/ExceptionsHandler.java
@@ -1,13 +1,18 @@
-package com.biwaby.financialtracker.exception;
+package com.biwaby.financialtracker.controller.advice;
 
 import com.biwaby.financialtracker.dto.response.ErrorResponse;
+import com.biwaby.financialtracker.exception.ResponseException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @Component
 @ControllerAdvice
@@ -19,17 +24,22 @@ public class ExceptionsHandler {
                 new ErrorResponse(
                         e.getMessage(),
                         HttpStatusCode.valueOf(e.getStatusCode()).toString(),
-                        e.getClass().getName()
+                        e.getExceptionClassName()
                 )
         );
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        BindingResult bindingResult = e.getBindingResult();
+        String errorMessages = bindingResult.getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(" "));
+        return ResponseEntity.status(e.getStatusCode()).body(
                 new ErrorResponse(
-                        e.getBindingResult().getAllErrors().getFirst().getDefaultMessage(),
-                        HttpStatus.BAD_REQUEST.toString(),
+                        errorMessages,
+                        e.getStatusCode().toString(),
                         e.getClass().getName()
                 )
         );
--- src/main/java/com/biwaby/financialtracker/exception/ResponseException.java
+++ src/main/java/com/biwaby/financialtracker/exception/ResponseException.java
@@ -7,6 +7,14 @@ public class ResponseException extends RuntimeException {
 
     private final Integer statusCode;
     private final String message;
+    private String exceptionClassName;
+
+    public ResponseException(Integer statusCode, String message, String exceptionClassName) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.exceptionClassName = exceptionClassName;
+    }
 
     public ResponseException(Integer statusCode, String message) {
         super(message);
