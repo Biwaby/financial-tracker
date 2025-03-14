2cc2c2aed12edcbcb1b72dee9916bcf5b5f94f9d
Andrey <456kam00@gmail.com>
2025-03-14 12:27:19 +0300

    feature: add toEditDto method in UserMapper

--- src/main/java/com/biwaby/financialtracker/mapper/UserMapper.java
+++ src/main/java/com/biwaby/financialtracker/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.biwaby.financialtracker.mapper;
 
 import com.biwaby.financialtracker.dto.UserDto;
+import com.biwaby.financialtracker.dto.UserEditDto;
 import com.biwaby.financialtracker.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
@@ -9,4 +10,5 @@ import org.mapstruct.MappingConstants;
 public interface UserMapper {
 
     UserDto toDto(User user);
+    UserEditDto toEditDto(User user);
 }
