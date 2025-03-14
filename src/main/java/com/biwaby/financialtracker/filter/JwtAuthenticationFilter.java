876c45ee56eac18a0e4d3f703ee73a98218e45f6
Andrey <456kam00@gmail.com>
2025-03-14 12:31:04 +0300

    feature: add JWT authentication filter

---
+++ src/main/java/com/biwaby/financialtracker/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,114 @@
+package com.biwaby.financialtracker.filter;
+
+import com.biwaby.financialtracker.exception.ResponseException;
+import com.biwaby.financialtracker.service.JwtService;
+import com.biwaby.financialtracker.service.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String HEADER_NAME = "Authorization";
+
+    @Autowired
+    private final JwtService jwtService;
+
+    @Autowired
+    private final UserService userService;
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private final HandlerExceptionResolver resolver;
+
+    public JwtAuthenticationFilter(
+            JwtService jwtService,
+            UserService userService,
+            @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver
+    ) {
+        this.jwtService = jwtService;
+        this.userService = userService;
+        this.resolver = resolver;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try {
+            var authHeader = request.getHeader(HEADER_NAME);
+
+            if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, BEARER_PREFIX)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            var jwt = authHeader.substring(BEARER_PREFIX.length());
+            var username = jwtService.extractUserName(jwt);
+
+            if (StringUtils.isNotEmpty(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userService
+                        .getUserDetailsService()
+                        .loadUserByUsername(username);
+
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    context.setAuthentication(authenticationToken);
+                    SecurityContextHolder.setContext(context);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {
+            resolver.resolveException(
+                    request,
+                    response,
+                    null,
+                    new ResponseException(
+                            HttpStatus.UNAUTHORIZED.value(),
+                            "JWT provided is expired",
+                            e.getClass().getName()
+                    )
+            );
+        } catch (Exception e) {
+            resolver.resolveException(
+                    request,
+                    response,
+                    null,
+                    new ResponseException(
+                            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                            "Unexpected error JWT processing request: %s".formatted(e.getMessage()),
+                            e.getClass().getName()
+                    )
+            );
+        }
+    }
+}
