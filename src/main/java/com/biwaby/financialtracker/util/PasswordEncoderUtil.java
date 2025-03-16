fca89b38a160c1072c3f1dd7f63b8cd23f936b19
Andrey <456kam00@gmail.com>
2025-03-16 19:32:25 +0300

    fix: remove component from PasswordEncoderUtil

--- src/main/java/com/biwaby/financialtracker/util/PasswordEncoderUtil.java
+++ src/main/java/com/biwaby/financialtracker/util/PasswordEncoderUtil.java
@@ -1,12 +1,8 @@
 package com.biwaby.financialtracker.util;
 
-import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
-@NoArgsConstructor
 public class PasswordEncoderUtil {
 
     public static PasswordEncoder getPasswordEncoder() {
